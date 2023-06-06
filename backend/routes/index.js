6fb606ab50ab85797456690cab3c78bcc0b6f55c
WJVincent <william@william-vincent.dev>
2023-06-06 17:42:27 -0400

    setup express

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+//take this route out when building the frontend
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+module.exports = router;
