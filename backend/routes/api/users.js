4c427eff3219a090807b511b990d5c1fe2ee5807
WJVincent <william@william-vincent.dev>
2023-06-06 19:21:49 -0400

    added log in route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -11,8 +11,37 @@ router.get('/current', (req, res) => {
 });
 
 //log in
-router.post('/current', (req, res) => {
+router.post('/current', async (req, res, next) => {
+    const { credential, password } = req.body;
 
+    const user = await User.unscoped().findOne({
+        where: {
+            [Op.or]: {
+                username: credential,
+                email: credential
+            }
+        }
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.password.toString())) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided credentials were invalid.' };
+        return next(err);
+    }
+
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+        user: safeUser
+    });
 });
 
 //sign up
